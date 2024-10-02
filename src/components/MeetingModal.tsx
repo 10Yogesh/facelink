2a2ae0f3a3739a0a84252ca6f1a35ef3f8498433
10Yogesh <yogeshkharel2@gmail.com>
2024-10-02 09:41:08 +0545

    Schedule Meeting Completed

--- src/components/MeetingModal.tsx
+++ src/components/MeetingModal.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import {
   Dialog,
   DialogContent,
@@ -16,7 +16,7 @@ interface MeetingModalProps {
   onClose: () => void;
   title: string;
   className?: string;
-  children?: string;
+  children?: ReactNode;
   handleClick?: () => void;
   buttonText?: string;
   image?: string;
